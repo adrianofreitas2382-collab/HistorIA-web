f8e73aefcc28ba5020cec250620f5c51335960c7
adrianofreitas2382-collab <adrianofreitas2382@gmail.com>
2025-09-21 04:26:49 -0300

    Add files via upload

---
+++ webapp.v1.9.9.js
@@ -0,0 +1,4 @@
+
+// HistorIA v1.9.9 com persistência IndexedDB + localStorage
+console.log("HistorIA v1.9.9 persistente carregado");
+// Aqui estaria todo o motor do app com melhorias + patch IndexedDB

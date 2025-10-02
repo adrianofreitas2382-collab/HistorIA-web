5ff98e9ef751092db8a4e08575f514df74c224a7
adrianofreitas2382-collab <adrianofreitas2382@gmail.com>
2025-10-02 00:02:05 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open('historia-v2').then(cache => cache.addAll([
-    '/', '/index.html', '/manifest.webmanifest',
-    '/assets/icon-192.png','/assets/icon-512.png',
-    '/webapp.v2.0.13.js'
-  ])));
-});
-self.addEventListener('activate', e => { self.clients.claim(); });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());

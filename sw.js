fe619f4d2e2b722674d469af5a81b264bae74c38
adrianofreitas2382-collab <adrianofreitas2382@gmail.com>
2025-10-12 03:37:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-
-const CACHE='historia-ia-shell-v2.1.0';const ASSETS=['./','./index.html','./styles.css','./webapp.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
+const CACHE='historia-ia-shell-v2.1.1';
+const ASSETS=['./','./index.html','./styles.css','./webapp.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))) });
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url); if(u.origin===location.origin){ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) } });
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
